6ebadd99c5ea4c29697c562ba2ebf7dd200a2b3e
m2en <merunno92@gmail.com>
2022-08-01 08:09:14 +0900

    feat: Add Gateway Intents

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,12 +2,16 @@ import dev.kord.core.Kord
 import dev.kord.core.event.gateway.ReadyEvent
 import dev.kord.core.event.message.MessageCreateEvent
 import dev.kord.core.on
+import dev.kord.gateway.Intent
+import dev.kord.gateway.Intents
 import io.github.cdimascio.dotenv.dotenv
 
 suspend fun main() {
     val dotenv = dotenv()
     val kord = Kord(dotenv.get("CITATION_BOT_TOKEN"))
-    kord.login()
+    kord.login() {
+        intents = Intents(Intent.Guilds, Intent.GuildMessages)
+    }
 
     kord.on<ReadyEvent> {
         println("Ready!")
