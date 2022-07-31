6a84fee14ab70e554a6d15092b8cbc3a2b1bb013
m2en <merunno92@gmail.com>
2022-08-01 03:54:00 +0900

    fix: remove token

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,9 +3,10 @@
 import dev.kord.core.Kord
 import dev.kord.core.event.gateway.ReadyEvent
 import dev.kord.core.on
+import utils.Util
 
 suspend fun main() {
-    val kord = Kord("MTAwMzM2Nzk4NDE2MjY3Mjc2MQ.GwPVzH.TTjUycLgQBjUZr_P2CkpddAUjj3GcNmXrzKnFU")
+    val kord = Kord(Util.getEnv("CITATION_BOT_TOKEN"))
     kord.login()
 
     kord.on<ReadyEvent> {
