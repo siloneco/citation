4db6cda77fb2e5312bf69eb6c7a72fb0c8095c73
m2en <merunno92@gmail.com>
2022-08-01 10:02:55 +0900

    fix: ignore message

--- src/main/kotlin/dev/m2en/citation/Main.kt
+++ src/main/kotlin/dev/m2en/citation/Main.kt
@@ -1,24 +1,29 @@
+package dev.m2en.citation
+
 import dev.kord.core.Kord
 import dev.kord.core.event.gateway.ReadyEvent
 import dev.kord.core.event.message.MessageCreateEvent
 import dev.kord.core.on
 import dev.kord.gateway.Intent
 import dev.kord.gateway.Intents
+import dev.kord.gateway.PrivilegedIntent
 import io.github.cdimascio.dotenv.dotenv
 
+@OptIn(PrivilegedIntent::class)
 suspend fun main() {
     val dotenv = dotenv()
     val kord = Kord(dotenv.get("CITATION_BOT_TOKEN"))
-    kord.login() {
-        intents = Intents(Intent.Guilds, Intent.GuildMessages)
-    }
 
     kord.on<ReadyEvent> {
-        println("Ready!")
+        println("citation is ready!")
     }
 
     kord.on<MessageCreateEvent> {
         if(message.author?.isBot == true && message.getGuildOrNull() == null) return@on
+    }
+
+    kord.login() {
+        intents = Intents(Intent.Guilds, Intent.GuildMessages, Intent.MessageContent)
     }
 }
 
